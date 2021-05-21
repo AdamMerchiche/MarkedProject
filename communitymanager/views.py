da1dc286b644e6a37ac847b9b4064a701dbe03af
Adam <amerchiche@gmail.com>
2021-05-21 16:56:17 +0200

    Impossible de faire marcher le bouton S'abonner.... Je n'arrive pas à, dans la vue statut, à exprimer l'ensemble des utilisateurs abonnés quand l'utilisaetur clique sur s'abonner. En appuyant sur un bouton, ca s'abonne à toutes les communautes

--- communitymanager/views.py
+++ communitymanager/views.py
@@ -1,39 +1,46 @@
 from django.contrib.auth import authenticate, login
 from .forms import *
 from django.contrib.auth import logout
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+
 
 
 # Create your views here.
 
 def communautes(request):
-    communautes = Communaute.objects.all()
-    for communaute in communautes:
-        if (request.user in communaute.abonnes.all()):  # finir ca ici
-            return render(request, 'communitymanager/communautes.html', {'communaute_abo': communautes})
-        else:
-            return render(request, 'communitymanager/communautes.html', {'communaute_abo': communautes})
+    communautes = Communaute.objects.all() # finir ca ici
+    return render(request, 'communitymanager/communautes.html', {'communaute_abo': communautes})
+
 
 
 def list_communautes(request):
     communautes = Communaute.objects.all()
     return render(request, 'communitymanager/list_communautes.html', {'communaute': communautes})
 
-
 def statut(request):
+    communautes = Communaute.objects.all()
+    is_subd = False
+    for communaute in communautes:
+        if communaute.abonnes.filter(username=request.user).exists():
+            communaute.abonnes.remove(request.user)
+            is_subd = False
+        else:
+            communaute.abonnes.add(request.user)
+            is_subd = True
+    return render(request, 'communitymanager/abonnement.html', {'communautes': communautes,'is_subd':is_subd})
+
+
+"""def statut(request):
     communautes = Communaute.objects.all()
     is_abonne = False
     for communaute in communautes:
-        print(communaute)
         if request.user in communaute.abonnes.all():
-            print(communaute.abonnes.all())
             is_abonne = False
-            communaute.abonnes == communaute.abonnes.exclude(username=request.user)
-        else:
+        if not request.user in communaute.abonnes.all():
             is_abonne = True
-            print(communaute.abonnes.all())
-            communaute.abonnes.all() == communaute.abonnes.add(request.user)
-            communaute.abonnes.all().save()
-    return render(request, 'communitymanager/abonnement.html', {'communautes': communautes, 'is_abonne': is_abonne})
+        print(is_abonne)
+    return render(request, 'communitymanager/abonnement.html', {'communautes': communautes,'is_abonne':is_abonne})"""
+
+
