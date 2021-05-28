cba63bb5ee44c41fe1c3a5f811576495b1ce833a
Guiglaz <guilhem.glaziou@gmail.com>
2021-05-28 21:05:27 +0200

    Boutons de modification de post retirés si le user n'est pas l'auteur et ajout de redirection vers la page du post créé ou modifié

--- communitymanager/views.py
+++ communitymanager/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Renvoie le feed d'un utilisateur, avec tous les posts des communautés auxquelles il est abonné
-@login_required
+@login_required(login_url='/accounts/login/')
 def accueil(request):
     communautes = Communaute.objects.filter(abonnes=request.user)
     posts = Post.objects.filter(communaute__abonnes=request.user)
@@ -13,14 +13,14 @@ def accueil(request):
 
 
 # Renvoie la liste de toutes les communautés
-@login_required
+@login_required(login_url='/accounts/login/')
 def liste_communautes(request):
     communautes = Communaute.objects.all()
     return render(request, 'communitymanager/list_communautes.html', {'communautes': communautes})
 
 
 # Permet à l'utilisateur de s'abonner ou se désabonner d'une communauté
-@login_required
+@login_required(login_url='/accounts/login/')
 def abonner(request, communaute_id):
     communaute = Communaute.objects.get(id=communaute_id)
     if request.user in communaute.abonnes.all():
@@ -31,7 +31,7 @@ def abonner(request, communaute_id):
 
 
 # Renvoie l'ensemble des posts d'une communauté précise
-@login_required
+@login_required(login_url='/accounts/login/')
 def communaute(request, communaute_id):
     return render(request, 'communitymanager/communaute.html',
                   {'posts': Post.objects.filter(communaute_id=communaute_id)})
@@ -39,7 +39,7 @@ def communaute(request, communaute_id):
 
 # Permet à l'utilisateur connecté de créer un commentaire. Il sera prérempli au niveau de la section Auteur,
 # et POST.
-@login_required
+@login_required(login_url='/accounts/login/')
 def commentaire(request, post_id):
     post = Post.objects.get(id=post_id)
     commentaires = Commentaire.objects.filter(post_id=post_id)
@@ -64,7 +64,7 @@ def commentaire(request, post_id):
 # Permet à l'utilisateur connecté de créer un POST. Il sera prérempli au niveau de la section Auteur,
 # et les choix de la communauté (lieu de publication) seront limités. L'utilisateur ne pourra poster que dans les
 # communautés auxquelles il est abonné.
-@login_required
+@login_required(login_url='/accounts/login/')
 def nouveau_post(request):
     form = PostForm(
         request.POST or None)
@@ -76,15 +76,16 @@ def nouveau_post(request):
                                          communautes]  # On limite la communauté où le POST sera partagé,
     # aux communautés auxquelles l'abonnée fait parti.
     if form.is_valid():
-        form.save()
+        post_cree = form.save()
         envoi = True
+        return redirect(commentaire, post_id=post_cree.id)
     return render(request, 'communitymanager/nouveau_post.html', locals())
 
 
 # Permet à l'utilisateur connecté de modifier son POST. La vue prend en compte l'auteur du post, et renvoie une erreur si
 # l'utilisateur tente de modifier un POST dont il n'est pas l'auteur. Le paramètre alert_flag renvoie un booléen. Il est FALSE
 # si la modification du POST est lancée par un autre utilisateur que l'auteur.
-@login_required
+@login_required(login_url='/accounts/login/')
 def modification_post(request, post_id):
     post = Post.objects.get(id=post_id)
     alert_flag = True
@@ -95,14 +96,14 @@ def modification_post(request, post_id):
             post = form.save()
             post.save()
             envoi = True
-
+            return redirect(commentaire, post_id=post.id)
     else:
         alert_flag = True
     return render(request, 'communitymanager/update_post.html', locals())
 
 
 # Permet de renvoyer tous les POSTs dont l'utilisateur est l'auteur.
-@login_required
+@login_required(login_url='/accounts/login/')
 def voir_posts(request):
     return render(
         request,
