e4eb5ecf2909e98ef91e2912b0b031b2be288f87
Adam <amerchiche@gmail.com>
2021-06-02 20:04:47 +0200

    Navbar fixé + description communauté + animation

--- communitymanager/forms.py
+++ communitymanager/forms.py
@@ -88,7 +88,7 @@ class CommunauteForm(forms.ModelForm):
 class ModificationCommunauteForm(forms.ModelForm):
     class Meta:
         model = Communaute
-        exclude = ["createur", "list_bannis", "abonnes", "ferme_invisible"] #Possible de modifier la description, le titre, et de bannir des abonnés de la commu
+        exclude = ["createur", "abonnes", "ferme_invisible"] #Possible de modifier la description, le titre, et de bannir des abonnés de la commu
 
 
 
